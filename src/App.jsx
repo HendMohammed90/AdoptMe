4e82938c48c84c028ead40c22d22b477a7db972c
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-05-03 18:01:11 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SearchParams from './pages/SearchParams';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+      <Routes>
+        <Route path='/' element={<SearchParams />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
