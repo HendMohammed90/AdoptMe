6c2cfd12bb26c8c0c659f7967e5359d2282c0f47
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-05-03 17:57:11 +0300

    add search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SearchParams from './components/SearchParams';
+import SearchParams from './pages/SearchParams';
 
 
 const App = () => {
