1cafffafbed22e079f53556e4e3c1675de1eff7b
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-05-14 20:46:49 +0300

    add petDetails

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import {React ,useEffect , useState} from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 
 function Details() {
   const {id} = useParams();
   const  navigate = useNavigate();
+  const  [petData, setPetData] = useState([]);
+  const [petImage, setPetImage ] = useState('http://pets-images.dev-apis.com/pets/none.jpg')
+
+  
+
+
+  const fetchPetDetails = async () => {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${id}`
+    );
+    const json = await res.json();
+    // console.log(json.pets[0].images[0])
+    setPetImage(json.pets[0].images[0])
+    setPetData(json.pets);
+  };
+
+  useEffect(() => {
+    fetchPetDetails();
+  }, [id]); 
+
   return (
     <div className='details'>
-      <h3>Pet id :{id}  </h3>
-      <button onClick={()=>{navigate("/")}}>Back</button>
+      {petData.map((petDetails) => (
+        <div value={petDetails} key={petDetails.id}>
+                <img src={petImage} alt={petDetails.name} className="image-container"/>
+                <h3>{petDetails.name} - {petDetails.animal} - {petDetails.breed} <br/>{petDetails.city}, {petDetails.state} </h3>
+                <p>{petDetails.description}</p>
+                <button onClick={()=>{navigate("/")}}>Adopt {petDetails.name}</button>
+                <br/>
+              </div>
+            ))}
+        <button onClick={()=>{navigate("/")}}>Back</button>
     </div>
   )
 }
