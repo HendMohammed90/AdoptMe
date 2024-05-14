74a326f98ce7c57bfe750540e7ca1858a5240dc7
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-05-14 19:04:52 +0300

    add useParams to get pet_id from the route

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
 function Details() {
+  const {id} = useParams();
+  const  navigate = useNavigate();
   return (
-    <div>
-      Details Page 
+    <div className='details'>
+      <h3>Pet id :{id}  </h3>
+      <button onClick={()=>{navigate("/")}}>Back</button>
     </div>
   )
 }
