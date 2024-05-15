b324d1c013d8f58fd58f0e0c5a96269248fc6986
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-05-15 20:26:52 +0300

    add react-loader-spinner library

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,23 +1,32 @@
 import {React ,useEffect , useState} from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import { Audio } from 'react-loader-spinner'
+import Loader from '../components/Loader';
 
 function Details() {
   const {id} = useParams();
   const  navigate = useNavigate();
   const  [petData, setPetData] = useState([]);
   const [petImage, setPetImage ] = useState('http://pets-images.dev-apis.com/pets/none.jpg')
-
-  
+  const [isLoading , setIsLoading] = useState(true);
+  const [error , setError] = useState(null)
 
 
   const fetchPetDetails = async () => {
-    const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?id=${id}`
-    );
-    const json = await res.json();
-    // console.log(json.pets[0].images[0])
-    setPetImage(json.pets[0].images[0])
-    setPetData(json.pets);
+    try {
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/pets?id=${id}`
+      );
+      const json = await res.json();
+      // console.log(json.pets[0].images[0])
+      setPetImage(json.pets[0].images[0])
+      setPetData(json.pets);
+      setIsLoading(false);
+      setError( null);
+    } catch (error) {
+      // console.log(error.message);
+      setError(error.message)
+    }
   };
 
   useEffect(() => {
@@ -26,7 +35,9 @@ function Details() {
 
   return (
     <div className='details'>
-      {petData.map((petDetails) => (
+      {/* {isLoading && <div>Data Is Loading....</div>} */}
+      {isLoading && <Loader/>}
+      {!isLoading && petData.map((petDetails) => (
         <div value={petDetails} key={petDetails.id}>
                 <img src={petImage} alt={petDetails.name} className="image-container"/>
                 <h3>{petDetails.name} - {petDetails.animal} - {petDetails.breed} <br/>{petDetails.city}, {petDetails.state} </h3>
@@ -35,6 +46,8 @@ function Details() {
                 <br/>
               </div>
             ))}
+      {error && <div>{error}</div>}
+      <br/>
         <button onClick={()=>{navigate("/")}}>Back</button>
     </div>
   )
