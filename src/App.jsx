234d84a248d66c845b3655ad1c605e4024c1c8a9
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-05-26 19:06:50 +0300

    using eact-query-devtools

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import SearchParams from './pages/SearchParams';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Details from './pages/Details';
 import  NotFound from './pages/NotFound';
 import './App.css'
 
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, // 1 minute
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      cacheTime: Infinity,
+      },
+      },
+})
 
 
 const App = () => {
@@ -25,6 +36,7 @@ const App = () => {
         <Route path='*' element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 };
