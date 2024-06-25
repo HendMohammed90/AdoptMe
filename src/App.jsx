40cc6e8ece7fef026c44fc8d67d8e20b7c1b57bb
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-06-25 14:36:52 +0300

    refactor: using lazy and Suspense for enhance preformance

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import SearchParams from './pages/SearchParams';
+import React, { useState , lazy, Suspense } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Details from './pages/Details';
-import NotFound from './pages/NotFound';
 import './App.css'
 import AdoptedPetContext from './contexts/AdoptedPetContext'
+import Loader from './components/Loader';
+
+
+// import SearchParams from './pages/SearchParams';
+// import Details from './pages/Details';
+// import NotFound from './pages/NotFound';
+
+// make use of lazy loading to enhance performance 
+const Details = lazy(() => import('./pages/Details'));
+const SearchParams = lazy(()=> import('./pages/SearchParams'));
+const NotFound = lazy(()=> import('./pages/NotFound'));
+
 
 
 // Create a client
@@ -23,13 +32,16 @@ const queryClient = new QueryClient({
 const App = () => {
 
   const adoptedPet = useState([]);
-  console.log(`adoptedPet is ${JSON.stringify(adoptedPet)}`)
 
   return (
     <BrowserRouter>
       <div id="modal"></div>
       <AdoptedPetContext.Provider value={adoptedPet}>
         <QueryClientProvider client={queryClient}>
+          {/* Show a spinner while app code is read more -> loading https://stackoverflow.com/a/63655226/6483379 */}
+          <Suspense fallback={<div className="loader-container">
+                <Loader />
+              </div>}>
           <header>
             <Link to="/" className='logo react'>Adopt Me!</Link>
           </header>
@@ -38,6 +50,7 @@ const App = () => {
             <Route path='/' element={<SearchParams />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
+          </Suspense>
         </QueryClientProvider>
       </AdoptedPetContext.Provider>
     </BrowserRouter>
